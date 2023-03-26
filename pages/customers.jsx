1e6d76ac381233d2ea3d8e56cd9f58eda25d8b73
Rosario Scarlata <rosarioscarlata24@gmail.com>
2023-03-26 12:43:41 -0300

    adding orders and modify customers

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -1,19 +1,20 @@
-import { FaShoppingBag } from "react-icons/fa";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import React from "react";
+import { BsPersonFill, BsThreeDotsVertical } from "react-icons/bs";
 import { data } from "../data/data.js";
+
 const customers = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className="flex justify-between px-4 pt-4">
-        <h2>Orders</h2>
-        <h2>Welcome Back, Clint</h2>
+      <div className="flex justify-between p-4">
+        <h2>Clientes</h2>
+        <h2>Bienvenido, Ro</h2>
       </div>
       <div className="p-4">
         <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
           <div className="my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
-            <span>Order</span>
-            <span className="sm:text-left text-right">Status</span>
-            <span className="hidden md:grid">Last Order</span>
+            <span>Nombre</span>
+            <span className="sm:text-left text-right">Email</span>
+            <span className="hidden md:grid">Ultima operacion</span>
             <span className="hidden sm:grid">Tipo</span>
           </div>
           <ul>
@@ -22,36 +23,20 @@ const customers = () => {
                 key={id}
                 className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer"
               >
-                <div className="flex">
+                <div className="flex items-center">
                   <div className="bg-purple-100 p-3 rounded-lg">
-                    <FaShoppingBag className="text-purple-800" />
+                    <BsPersonFill className="text-purple-800" />
                   </div>
-                  <div className="pl-4">
-                    <p className="text-gray-800 font-bold">
-                      ${order.total.toLocaleString()}
-                    </p>
-                    <p className="text-gray-800 text-sm">{order.name.first}</p>
-                  </div>
+                  <p className="pl-4">
+                    {order.name.first + " " + order.name.last}
+                  </p>
                 </div>
                 <p className="text-gray-600 sm:text-left text-right">
-                  <span
-                    className={
-                      order.status == "Processing"
-                        ? "bg-green-200 p-2 rounded-lg"
-                        : order.status == "Completed"
-                        ? "bg-blue-200 p-2 rounded-lg"
-                        : "bg-yellow-200 p-2 rounded-lg"
-                    }
-                  >
-                    {order.tipo}
-                  </span>
+                  {order.name.first}@gmail.com
                 </p>
                 <p className="hidden md:flex">{order.date}</p>
                 <div className="sm:flex hidden justify-between items-center">
-                  <span>
-                    <p>{order.status}</p>
-                  </span>
-
+                  <p>{order.tipo}</p>
                   <BsThreeDotsVertical />
                 </div>
               </li>
@@ -62,4 +47,5 @@ const customers = () => {
     </div>
   );
 };
+
 export default customers;
--- pages/orders.jsx
+++ pages/orders.jsx
@@ -1,3 +1,65 @@
-export const orders = () => {
-  return <div>orders</div>;
+import { FaShoppingBag } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { data } from "../data/data.js";
+const customers = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="flex justify-between px-4 pt-4">
+        <h2>Operaciones</h2>
+        <h2>Bienvenido, Ro</h2>
+      </div>
+      <div className="p-4">
+        <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
+          <div className="my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
+            <span>Order</span>
+            <span className="sm:text-left text-right">Tipo</span>
+            <span className="hidden md:grid">Ultima op</span>
+            <span className="hidden sm:grid">Estado</span>
+          </div>
+          <ul>
+            {data.map((order, id) => (
+              <li
+                key={id}
+                className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer"
+              >
+                <div className="flex">
+                  <div className="bg-purple-100 p-3 rounded-lg">
+                    <FaShoppingBag className="text-purple-800" />
+                  </div>
+                  <div className="pl-4">
+                    <p className="text-gray-800 font-bold">
+                      ${order.total.toLocaleString()}
+                    </p>
+                    <p className="text-gray-800 text-sm">{order.name.first}</p>
+                  </div>
+                </div>
+                <p className="text-gray-600 sm:text-left text-right">
+                  <span
+                    className={
+                      order.tipo == "Venta"
+                        ? "bg-green-200 p-2 rounded-lg"
+                        : order.tipo == "Compra"
+                        ? "bg-blue-200 p-2 rounded-lg"
+                        : "bg-yellow-200 p-2 rounded-lg"
+                    }
+                  >
+                    {order.tipo}
+                  </span>
+                </p>
+                <p className="hidden md:flex">{order.date}</p>
+                <div className="sm:flex hidden justify-between items-center">
+                  <span>
+                    <p>{order.status}</p>
+                  </span>
+
+                  <BsThreeDotsVertical />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
+export default customers;
